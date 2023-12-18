604d8b5455f5a229933bc46761bab6c03f2d25c9
Oleg Kravhcuk <o.kravchuk@btc-s.ru>
2023-12-18 12:01:31 +0600

    feat: remove tests

--- src/test/kotlin/com/study/financial/FinancialApplicationTests.kt
+++ src/test/kotlin/com/study/financial/FinancialApplicationTests.kt
@@ -1,13 +1,13 @@
-package com.study.financial
-
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class FinancialApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
-}
+//package com.study.financial
+//
+//import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.context.SpringBootTest
+//
+//@SpringBootTest
+//class FinancialApplicationTests {
+//
+//	@Test
+//	fun contextLoads() {
+//	}
+//
+//}
