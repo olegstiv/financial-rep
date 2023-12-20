d6748c0575a22f3ba06ad040e70f1ec9fcda3035
Oleg Kravhcuk <o.kravchuk@btc-s.ru>
2023-12-20 19:42:05 +0600

    feat: add server

--- src/main/kotlin/com/study/financial/configuration/OpenApiConfig.kt
+++ src/main/kotlin/com/study/financial/configuration/OpenApiConfig.kt
@@ -5,16 +5,24 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SwaggerConfiguration {
+
+    @Value("\${server.url}")
+    lateinit var serverUrl: String
+
     @Bean
     fun customOpenAPI(): OpenAPI {
         val securitySchemeName = "Auth JWT"
+        val server = Server().url(serverUrl)
         return OpenAPI()
             .addSecurityItem(SecurityRequirement().addList(securitySchemeName))
+            .addServersItem(server)
             .components(
                 Components()
                     .addSecuritySchemes(
@@ -26,6 +34,6 @@ class SwaggerConfiguration {
                             .bearerFormat("JWT"),
                     ),
             )
-            .info(Info().title("Wayne's Talk APIs").version("v1.0.0"))
+            .info(Info().title("Study project for financial").version("v1.0.0"))
     }
 }
